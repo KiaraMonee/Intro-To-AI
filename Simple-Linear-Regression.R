36dbcd6bc1be731538b94c199d6252f788f9c591
Kiara Mikel <112020040+KiaraMonee@users.noreply.github.com>
2025-09-02 14:53:46 -0400

    Add files via upload

--- Simple-Linear-Regression.R
+++ Simple-Linear-Regression.R
@@ -1,1 +1,17 @@
 #Kiara Mikel; Aug 28, 2025; Test simple linear regression
+
+#Get some dummy dataset
+cars <- mtcars 
+#head(cars),dim(cars)
+
+#Scatter plot on training data
+scatter.smooth(x=cars$mpg, y=cars$disp, main="SpeedVSdistance")
+
+#Calculate correlation value between mpg and disp
+cor(cars$mpg, cars$disp)
+
+#Split training and testing data for regression
+training_data <- cars[1:16,]
+test_data <- cars[17:32,]
+
+#Build a regression model
